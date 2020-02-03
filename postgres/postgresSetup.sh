8fd53fdc80bce895bf821ef4ce8bdeba0ec3fc99
Asam Shan <asamshan456@gmail.com>
2020-02-03 23:29:19 +0000

    adding some output so user can nuderstand what vars are used or missing

--- postgres/postgresSetup.sh
+++ postgres/postgresSetup.sh
@@ -2,7 +2,26 @@
 
 if [[ -n "${DATABASE_ENDPOINT+set}" && -n "${DATABASE_USER+set}" && -n "${DATABASE_PASSWORD+set}" ]];
 then
+echo "Creating users table. Database: $DATABASE_ENDPOINT, user: $DATABASE_USER"
   psql "dbname=$DATABASE_ENDPOINT user=$DATABASE_USER password=$DATABASE_PASSWORD" -f ./CreateTable.sql
 else
-  echo "Variables are missing"
+    if [[ ! -n "${DATABASE_ENDPOINT+set}" ]]
+    then
+      db="DATABASE_ENDPOINT"
+    fi
+    if [[ ! -n "${DATABASE_USER+set}" ]]
+    then
+      us="DATABASE_USER"
+    fi
+    if [[ ! -n "${DATABASE_PASSWORD+set}" ]]
+    then
+    pw="DATABASE_PASSWORD"
 fi
+  echo -e "Variables are missing.\nHere they are:\n$db \n$us \n$pw \nBye"
+fi
+
+
+
+
+
+

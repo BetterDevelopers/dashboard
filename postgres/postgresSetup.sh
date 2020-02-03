5effe810ccb1ab6af5bc071748d4501469634a8b
Asam Shan <asamshan456@gmail.com>
2020-02-03 22:38:45 +0000

    adding validation for creds

--- postgres/postgresSetup.sh
+++ postgres/postgresSetup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
- psql "dbname=$DATABASE_ENDPOINT user=$DATABASE_USER password=$DATABASE_PASSORD" -f ./CreateTable.sql
+
+if [[ -n "${DATABASE_ENDPOINT+set}" && -n "${DATABASE_USER+set}" && -n "${DATABASE_PASSWORD+set}" ]];
+then
+  psql "dbname=$DATABASE_ENDPOINT user=$DATABASE_USER password=$DATABASE_PASSWORD" -f ./CreateTable.sql
+else
+  echo "Variables are missing"
+fi

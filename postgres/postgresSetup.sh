fb2184cbcf3b7da451b9d59dc80c9a52281a87f1
Asam Shan <asamshan456@gmail.com>
2020-02-03 23:43:20 +0000

    refactoring ! -n to -z and correcting indentation

--- postgres/postgresSetup.sh
+++ postgres/postgresSetup.sh
@@ -2,20 +2,20 @@
 
 if [[ -n "${DATABASE_ENDPOINT+set}" && -n "${DATABASE_USER+set}" && -n "${DATABASE_PASSWORD+set}" ]];
 then
-echo "Creating users table. Database: $DATABASE_ENDPOINT, user: $DATABASE_USER"
+  echo "Creating users table. Database: $DATABASE_ENDPOINT, user: $DATABASE_USER"
   psql "dbname=$DATABASE_ENDPOINT user=$DATABASE_USER password=$DATABASE_PASSWORD" -f ./CreateTable.sql
 else
-    if [[ ! -n "${DATABASE_ENDPOINT+set}" ]]
-    then
-      db="DATABASE_ENDPOINT"
-    fi
-    if [[ ! -n "${DATABASE_USER+set}" ]]
-    then
-      us="DATABASE_USER"
-    fi
-    if [[ ! -n "${DATABASE_PASSWORD+set}" ]]
-    then
-    pw="DATABASE_PASSWORD"
+if [[ -z "${DATABASE_ENDPOINT+set}" ]]
+then
+  db="DATABASE_ENDPOINT"
+fi
+if [[ -z "${DATABASE_USER+set}" ]]
+then
+  us="DATABASE_USER"
+fi
+if [[ -z "${DATABASE_PASSWORD+set}" ]]
+then
+pw="DATABASE_PASSWORD"
 fi
   echo -e "Variables are missing.\nHere they are:\n$db \n$us \n$pw \nBye"
 fi
